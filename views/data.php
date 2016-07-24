b6e335918883a25429ecee60bf49f19a0c17e2dd
Nicholas Bullington <nicholas.e.bullington@gmail.com>
2016-07-24 20:35:12 +0000

    July 24th status

--- views/data.php
+++ views/data.php
@@ -11,7 +11,7 @@
 <form action="slurryinfo.php" method="post">
 <fieldset>
 <div class="row">
-    <div class="col-xs-4 col-xs-offset-2 text-left">
+    <div class="col-xs-3 col-xs-offset-1 text-left">
         <select  name="district" class="selectpicker" title="Select Job District.." data-size="8" data-live-search="true">
             <?php
                 foreach ($districts as $district) 
@@ -22,10 +22,7 @@
             ?>
         </select>
     </div>
-</div>
-<br/><br/>
-<div class="row">
-    <div class="col-xs-4 col-xs-offset-2 text-left">
+    <div class="col-xs-3 col-xs-offset-1 text-left">
         <select  name="chosen_company" class="selectpicker" title="Select Customer Name.." data-size="12" data-live-search="true">
             <?php 
                 foreach ($options as $option) 
@@ -36,16 +33,17 @@
             ?>
         </select>
     </div>
-    <div class="col-xs-5 col-xs-offset-1">
+    <div class="col-xs-3">
         <span class="pull-left">
             <a href="newcompany.php" class="btn my-btn col-xs-offset-1" role="button">Add New Company</a>
         </span>
     </div>
 </div>    
-<br/><br/>
+<br/>
 <div class="row">
-    <div class="col-xs-4 col-xs-offset-2 text-left">
-        <select  name="job_type" class="selectpicker" title="Select Job Type.." data-size="8">
+    <div class="col-xs-3 col-xs-offset-1 text-left">
+        <br/>
+            <select  name="job_type" class="selectpicker" title="Select Job Type.." data-size="8">
             <optgroup label = "Primary Jobs">
                 <?php
                     foreach ($jobs as $job) 
@@ -72,10 +70,10 @@
             </optgroup>
         </select>
     </div>
-    <div class="col-xs-5 col-xs-offset-1 text-left">
+    <div class="col-xs-3 col-xs-offset-1 text-left">
         Number of Stages:
         <div class="row">
-            <div class="col-xs-5 text-left">
+            <div class="col-xs-3 text-left">
                 <select  name="stage_count" class="selectpicker" data-size="12">
                     <option value="1">1</option>
                     <option value="2">2</option>
@@ -87,24 +85,47 @@
 </div>
 <br/>
 <div class="row">
-    <div class="col-xs-4 col-xs-offset-2 text-left"> Enter Well Name:
+    <div class="col-xs-3 col-xs-offset-1 text-left"> Enter Well Name:
         <div class="row">
             <div id="prefetch_name" class="col-xs-5 text-left">
                 <input name="well" class="typeahead form-control" type="text" placeholder="Well Name">
             </div>
         </div>
     </div>
-    <div class="col-xs-5 col-xs-offset-1 text-left"> Enter Well Number:
+    <div class="col-xs-3 col-xs-offset-1 text-left"> Enter Well Number:
         <div class="row">
             <div id="prefetch_number" class="col-xs-5 text-left">
                 <input name="number" class="typeahead form-control" type="text" placeholder="Well Number">
             </div>
         </div>
     </div>
-</div>    
-<br/><br/>
+</div>
+<br/>
 <div class="row">
-    <div class="col-xs-4 col-xs-offset-2 text-left">
+    <div class="col-xs-3 col-xs-offset-1 text-left"> Select Supervisor:
+        <div class="row">
+            <div id="prefetch_name" class="col-xs-5 text-left">
+                <input name="well" class="typeahead form-control" type="text" placeholder="Well Name">
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-3 col-xs-offset-1 text-left"> Select Pump Operator:
+        <div class="row">
+            <div id="prefetch_number" class="col-xs-5 text-left">
+                <input name="number" class="typeahead form-control" type="text" placeholder="Well Number">
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-3">
+        <br/>
+        <span class="pull-left">
+            <a href="#" class="btn my-btn col-xs-offset-1" role="button">Add Field Employee</a>
+        </span>
+    </div>
+</div>    
+<br/>
+<div class="row">
+    <div class="col-xs-4 col-xs-offset-1 text-left">
         Total Number of Slurries:
         <div class="row">
             <div class="col-xs-5 text-left">
